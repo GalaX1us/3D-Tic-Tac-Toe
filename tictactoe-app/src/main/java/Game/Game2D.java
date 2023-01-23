48f14e3edce54bf34b37d3a4a00c01e5d64f64f7
p2007049 <luc.brun@etu.univ-lyon1.fr>
2023-01-23 16:00:14 +0100

    affichage 2D

--- tictactoe-app/src/main/java/Game/Game2D.java
+++ tictactoe-app/src/main/java/Game/Game2D.java
@@ -1,4 +1,53 @@
 package Game;
-public class Game2D {
+
+import Player.Human;
+import Player.Player;
+import Game.Cell;
+
+
+public class Game2D extends Game{
+
+    private int gridSize;
+    public Cell[][]grid;
+
+
+    public Game2D(int size) {
+        this.player1 = new Human();
+        this.player2 = new Human();
+        this.currentPlayer = 1;
+
+        this.gridSize = size;
+        this.grid = new Cell[size][size];
+
+        this.initGrid();
+    }
+
+    private void initGrid(){
+        for (int line = 0; line < this.gridSize; line++) {
+            for (int column = 0; column < this.gridSize; column++) {
+                this.grid[line][column] = new Cell(line*this.gridSize+column+1); //verifier que ça marche bien ici 
+            }
+        }
+    }
+
+    public void displayGrid(){
+        for (int line = 0; line < this.gridSize; line++) {
+            for (int column = 0; column < this.gridSize; column++) {
+                this.grid[line][column].displayCell();  
+                if (column<this.gridSize-1) {
+                    System.out.print("|"); 
+                }
+            }
+            System.out.println();
+            if (line<this.gridSize-1) {
+                for (int i = 0; i < this.gridSize+(3*(this.gridSize-1)); i++) {
+                    System.out.print("-"); 
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
     
 }
--- tictactoe-app/src/main/java/com/tictactoe/app/App.java
+++ tictactoe-app/src/main/java/com/tictactoe/app/App.java
@@ -2,6 +2,7 @@ package com.tictactoe.app;
 
 import Game.Game;
 import Game.Game3D;
+import Game.Game2D;
 
 /**
  * Hello world!
@@ -12,7 +13,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Game g = new Game3D(3);
+        Game g = new Game2D(3);
         g.displayGrid();
     }
 }
